8e9a647205b64b45c082bcd63a1d558a2688417e
Anton Shapoval <antonshapoval2002@gmail.com>
2021-02-02 16:26:52 +0200

    Project without styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import classes from "./App.module.css";
+import WeatherDisplay from "./Components/WeatherDisplay/WeatherDisplay";
 
-const App = () =>  {
-    return (
-      <>
-      
-      </>
-    );
-}
+const App = () => {
+  const places = ["Kharkiv", "Kiev", "Lviv", "London"];
+  const [activeCity, setActiveCity] = useState(0);
+  return (
+    <div className={classes.App}>
+      <header className={classes.appHeader}>
+        {places.map((city, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => {
+                setActiveCity(index);
+              }}
+            >
+              {city}
+            </button>
+          );
+        })}
+      </header>
+
+      <WeatherDisplay key={activeCity} name={places[activeCity]} />
+    </div>
+  );
+};
 
 export default App;
